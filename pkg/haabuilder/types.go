530ed13db304504fa290f13200f8ac1f68fc942e
Christophe Fergeau <cfergeau@redhat.com>
2024-04-27 16:09:09 +0200

    start adding haabuilder -> arkhamdb cycle mapping

---
+++ pkg/haabuilder/types.go
@@ -0,0 +1,85 @@
+package haabuilder
+
+const baseURL = "https://haa.cgbuilder.fr/liste_carte/"
+// 1 -> core
+
+// 2 -> dwl
+// 3 -> dwl tmm
+// 4 -> dwl tece
+// 5 -> dwl bota
+// 6 -> dwl uau
+// 7 -> dwl wda
+// 8 -> dwl litas
+
+// 9 -> ptc
+// 10 -> ptc eotp
+// 11 -> ptc tuo
+// 12 -> ptc apot
+// 13 -> ptc tpm
+// 14 -> ptc bsr
+// 15 -> ptc dca
+
+// 16 -> tfa
+// 17 -> tfa tof
+// 18 -> tfa tbb
+// 19 -> tfa hote
+// 20 -> tfa tcoa
+// 21 -> tfa tdoy
+// 22 -> tfa sha
+
+// 23 -> tcu
+// 24 -> tcu tsn
+// 25 -> tcu wos
+// 26 -> tcu fgg
+// 27 -> tcu uad
+// 28 -> tcu icc
+// 29 -> tcu bbt
+
+// 30 -> tda
+// 31 -> tda sfk
+// 32 -> tda tsh
+// 36 -> tda dsm
+// 37 -> tda pnr
+// 38 -> tda wgd
+// 39 -> tda woc
+
+// 46 -> tic 
+// 54 -> tic itd
+// 55 -> tic def
+// 57 -> tic hhg
+// 58 -> tic lif
+// 59 -> tic lod
+// 60 -> tic itm
+
+// 62 -> eoe eoec
+// 63 -> eoe eoep
+
+// 66 -> tsk tskc
+// 65 -> tsk tskp
+
+// 68 -> fhv fhvc
+// 69 -> fhv fhvp
+
+// 33 -> rtnotz
+// 34 -> rtdwl
+// 35 -> rtptc
+// 40 -> rttfa
+// 61 -> rttcu
+
+// 47 -> guardians
+// 48 -> hotel
+// 49 -> blob
+// 50 -> lol
+// 51 -> cotr
+// 52 -> coh
+// 53 -> La Cabale de Myarlathotep -> not in arkhamdb
+// 56 -> wog
+// 64 -> mtt
+// 67 -> fof
+
+
+// 45 -> nat
+// 44 -> har
+// 43 -> win
+// 42 -> jac
+// 41 -> ste
