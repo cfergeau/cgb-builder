363ff463b2b371dc22d25d39753ffceebe32260f
Christophe Fergeau <cfergeau@redhat.com>
2024-04-27 14:58:24 +0200

    fixup! Parsing almost done + json serialization

--- pkg/html/html.go
+++ pkg/html/html.go
@@ -29,7 +29,7 @@ func FetchURL(url string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-func findAttr(node *html.Node, attrName string) string {
+func FindAttr(node *html.Node, attrName string) string {
 	for _, a := range node.Attr {
 		if a.Key == attrName {
 			return a.Val
@@ -39,8 +39,17 @@ func findAttr(node *html.Node, attrName string) string {
 	return ""
 }
 
+func HasAttrWithValue(node *html.Node, attrName, value string) bool {
+        attrValue := FindAttr(node, attrName)
+        if attrValue == "" {
+                return false
+        }
+
+        return attrValue == value
+}
+
 func GetClasses(node *html.Node) []string {
-	return strings.Split(findAttr(node, "class"), " ")
+	return strings.Split(FindAttr(node, "class"), " ")
 }
 
 func HasClass(node *html.Node, class string) bool {
@@ -49,11 +58,14 @@ func HasClass(node *html.Node, class string) bool {
 }
 
 func GetId(node *html.Node) string {
-	return findAttr(node, "id")
+	return FindAttr(node, "id")
 }
 
 func FindNodes(root *html.Node, match func(*html.Node) bool) []*html.Node {
 	matches := []*html.Node{}
+        if match(root) {
+                matches = append(matches, root)
+        }
 	for c := root.FirstChild; c != nil; c = c.NextSibling {
 		if match(c) {
 			matches = append(matches, c)
@@ -71,6 +83,9 @@ func FindNodes(root *html.Node, match func(*html.Node) bool) []*html.Node {
 }
 
 func FindNode(root *html.Node, match func(*html.Node) bool) *html.Node {
+        if match(root) {
+                return root
+        }
 	for c := root.FirstChild; c != nil; c = c.NextSibling {
 		if match(c) {
 			return c
